fa02f04f9ee4f1d454c0cc16983f243673f23cc5
Quinton <davissql@miamioh.edu>
2026-05-05 11:28:45 -0400

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -1,33 +1,36 @@
 module regfile (
     input clk,
+    input reset,
     input [4:0] rs1,
     input [4:0] rs2,
     input [4:0] rd,
     input [31:0] wd,
     input we,
 
-    output [31:0] rd1,
-    output [31:0] rd2,
+    input [4:0] debug_reg,
+    output [31:0] debug_data,
 
-    output [31:0] reg1_out,
-    output [31:0] reg2_out,
-    output [31:0] reg3_out,
-    output [31:0] reg6_out
+    output [31:0] rd1,
+    output [31:0] rd2
 );
 
     reg [31:0] regs [0:31];
+    integer i;
 
     assign rd1 = (rs1 == 0) ? 0 : regs[rs1];
     assign rd2 = (rs2 == 0) ? 0 : regs[rs2];
 
-    assign reg1_out = regs[1];
-    assign reg2_out = regs[2];
-    assign reg3_out = regs[3];
-    assign reg6_out = regs[6];
+    assign debug_data = (debug_reg == 0) ? 0 : regs[debug_reg];
 
-    always @(posedge clk) begin
-        if (we && rd != 0)
-            regs[rd] <= wd;
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            for (i = 0; i < 32; i = i + 1)
+                regs[i] <= 0;
+        end
+        else begin
+            if (we && rd != 0)
+                regs[rd] <= wd;
+        end
     end
 
 endmodule
