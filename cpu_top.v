bfec38c4c8b481157a8cb6bb17d00119fcf580e7
Quinton <davissql@miamioh.edu>
2026-05-05 11:28:13 -0400

    Update cpu_top.v

--- cpu_top.v
+++ cpu_top.v
@@ -2,36 +2,62 @@ module cpu_top (
     input        CLOCK_50,
     input  [3:0] KEY,
     input  [9:0] SW,
-    output [9:0] LEDR
+    output [9:0] LEDR,
+    output [6:0] HEX0,
+    output [6:0] HEX1
 );
 
     wire rst = ~KEY[3];
+	 wire mem_select = SW[7];
 
     reg [25:0] counter;
 
-    always @(posedge CLOCK_50) begin
-        counter <= counter + 1;
+    always @(posedge CLOCK_50 or posedge rst) begin
+        if (rst)
+            counter <= 0;
+        else
+            counter <= counter + 1;
     end
 
     wire slow_clk = counter[25];
 
     wire [31:0] pc;
-    wire [31:0] r1, r2, r3, r6;
+    wire [31:0] debug_data;
+    wire all_passed;
+
+    wire [4:0] selected_reg;
+    assign selected_reg = SW[4:0];
 
     cpu CPU (
-        .clk(slow_clk),
-        .reset(rst),
-        .pc_debug(pc),
-        .reg1_debug(r1),
-        .reg2_debug(r2),
-        .reg3_debug(r3),
-        .reg6_debug(r6)
+    .clk(slow_clk),
+    .reset(rst),
+    .mem_select(mem_select),
+
+    .debug_reg(selected_reg),
+    .debug_data(debug_data),
+    .pc_debug(pc),
+    .all_passed(all_passed)
+);
+
+    wire [31:0] display_value;
+	 assign display_value = (SW[8] == 1'b1) ? pc : debug_data;
+	 
+    assign LEDR[8:0] = display_value[8:0];
+
+    assign LEDR[9] = all_passed;
+
+    // HEX0 and HEX1 show low byte of selected value
+    seg7 S0 (
+        .value(display_value[3:0]),
+        .o(HEX0)
     );
 
-    assign LEDR =
-        (SW[1:0] == 2'b00) ? pc[9:0] :
-        (SW[1:0] == 2'b01) ? r1[9:0] :
-        (SW[1:0] == 2'b10) ? r3[9:0] :
-                             r6[9:0];
+    seg7 S1 (
+        .value(display_value[7:4]),
+        .o(HEX1)
+    );
+
+  
+  
 
 endmodule
