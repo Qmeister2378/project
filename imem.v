55ac950b9908816c89d67f9acad0d8cab196ed82
Quinton <davissql@miamioh.edu>
2026-05-05 11:28:28 -0400

    Update imem.v

--- imem.v
+++ imem.v
@@ -1,14 +1,18 @@
 module imem (
     input [31:0] addr,
+    input mem_select,
     output [31:0] data
 );
 
-    reg [31:0] memory [0:255];
+    reg [31:0] memory0 [0:255];
+    reg [31:0] memory1 [0:255];
 
     initial begin
-        $readmemh("program.mem", memory);
+        $readmemh("program.mem", memory0);
+        $readmemh("program.mem", memory1);
     end
 
-    assign data = memory[addr[9:2]];
+    assign data = (mem_select == 1'b0) ? memory0[addr[9:2]] :
+                                           memory1[addr[9:2]];
 
 endmodule
